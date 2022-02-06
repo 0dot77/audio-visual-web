9e11fafd4f7681d77d90850d3e50099c165d09c6
TAEYANG YOO <yty0706@gmail.com>
2022-02-06 22:16:21 +0900

    스크롤이벤트만드는중

--- js/nav.js
+++ js/nav.js
@@ -1,5 +1,17 @@
+'use strict';
 /*
 네비게이션바를 스크롤링 하면,
 폰트 크기, 박스 크기, 줄어들면서 위치에 안착되도록 하기
 */
 
+// 스크롤 하면 폰트의 사이즈가 줄어드는 이벤트 만들어보기
+
+let text = document.getElementById('title');
+// console.log(text.style);
+
+function textSizing(e) {
+    let sy = e.path[1].scrollY;
+    text.style.fontSize = sy;
+}
+
+window.addEventListener('scroll', textSizing);
