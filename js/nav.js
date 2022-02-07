a4569738a58652937dcecf5e5b81e4d1ff6fa8a8
TAEYANG YOO <yty0706@gmail.com>
2022-02-07 23:18:04 +0900

    scroll 이벤트 만들기

--- js/nav.js
+++ js/nav.js
@@ -5,13 +5,36 @@
 */
 
 // 스크롤 하면 폰트의 사이즈가 줄어드는 이벤트 만들어보기
+// 갑자기 아래 쪽으로 스냅이 되는 문제를 해결해야할 것 같음
 
-let text = document.getElementById('title');
-// console.log(text.style);
+let titleText = document.getElementById('title');
+let screenNav = document.querySelector('.screen-nav');
 
-function textSizing(e) {
-    let sy = e.path[1].scrollY;
-    text.style.fontSize = sy;
+let defaultTitleTextSize = 300;
+let scrollValue;
+
+// 스크롤 -> 텍스트 사이즈 줄여주기
+titleText.style.fontSize = `${defaultTitleTextSize}px`;
+
+const titleScroll = function(windowValue) {
+    // 기본 값으로 scroll 값 설정하고,
+    // 일정 값까지 가기 전까지는 쭉 빼주고, 나머지는 값 저장하기
+    scrollValue = windowValue.path[1].scrollY;
+
+    if(scrollValue < 100) {
+        titleText.style.fontSize = `${defaultTitleTextSize}px`;
+    } else if(scrollValue > 100 && scrollValue < 300) {
+        titleText.style.fontSize = `${defaultTitleTextSize-scrollValue}px`;
+    }
+    // console.log(scrollValue);
+};
+
+// 네비게이션 자체의 height를 쭉 줄이기
+
+const navHeight = function(windowValue) {
+    scrollValue = windowValue.path[1].scrollY;
+    console.log(screenNav.style);
 }
 
-window.addEventListener('scroll', textSizing);
+window.addEventListener('scroll', titleScroll);
+window.addEventListener('scroll', navHeight);
